d63054b9420eb977a532340c34d1b66dbdf5977e
emils <esla22@student.aau.dk>
2026-02-05 14:09:07 +0100

    ran black formatter and made a signature for ranked choice

--- voting-app/algorithms.py
+++ voting-app/algorithms.py
@@ -1,5 +1,6 @@
 from itertools import permutations
 
+
 def parse_votes(votes, options):
     """Convert CSV vote rows to usable format"""
     parsed = []
@@ -14,11 +15,12 @@ def parse_votes(votes, options):
 
 # ============== METHODS ==============
 
+
 def schulze_method(parsed_votes, option_names):
     """Schulze/Beatpath method"""
     # preferences maps (A,B) to the number of voters who prefer A to B. Everything starts at 0.
-    preferences = {(A,B) : 0 for A in option_names for B in option_names if A != B}
-    
+    preferences = {(A, B): 0 for A in option_names for B in option_names if A != B}
+
     # fill out the preferences
     for dict in parsed_votes:
         # username = dict["username"]
@@ -26,33 +28,46 @@ def schulze_method(parsed_votes, option_names):
         for option_a, score_a in scores:
             for option_b, score_b in scores:
                 if option_a != option_b and score_a > score_b:
-                    preferences[(option_a,option_b)] += 1
+                    preferences[(option_a, option_b)] += 1
 
     # implementation of strongest path strength computation from https://en.wikipedia.org/wiki/Schulze_method
     path_strength = {}
     for i in option_names:
         for j in option_names:
             if i != j:
-                path_strength[i,j] = preferences[i,j] - preferences[j,i]
+                path_strength[i, j] = preferences[i, j] - preferences[j, i]
 
     for k in option_names:
         for i in option_names:
             if i != k:
                 for j in option_names:
                     if j != k and j != i:
-                        path_strength[i,j] = max(path_strength[i,j], min(path_strength[i,k], path_strength[k,j]))
-                        
+                        path_strength[i, j] = max(
+                            path_strength[i, j],
+                            min(path_strength[i, k], path_strength[k, j]),
+                        )
+
     # ranking maps options to the number of things they are preferred to.
-    ranking = {option : 0 for option in option_names}  # highest rank is the best one
+    ranking = {option: 0 for option in option_names}  # highest rank is the best one
     for option_a in option_names:
         for option_b in option_names:
             if option_a != option_b:
-                if path_strength[(option_a,option_b)] >= path_strength[(option_b,option_a)]:
+                if (
+                    path_strength[(option_a, option_b)]
+                    >= path_strength[(option_b, option_a)]
+                ):
                     # A is better than B
                     ranking[option_a] += 1
 
-    return tiebreak_with_total_scores(parsed_votes, sorted(ranking.items(), reverse=True, key=lambda x: x[1]))
-    
+    return tiebreak_with_total_scores(
+        parsed_votes, sorted(ranking.items(), reverse=True, key=lambda x: x[1])
+    )
+
+
+def ranked_choice(parsed_votes, option_names):
+    pass
+
+
 def tiebreak_with_total_scores(parsed_votes, ranked_items):
     """Sort a list of ranked voting options using the total score given to them by voters."""
     total_scores = {}
@@ -60,7 +75,10 @@ def tiebreak_with_total_scores(parsed_votes, ranked_items):
         scores = dict["scores"].items()
         for option, score in scores:
             total_scores[option] = total_scores.get(option, 0) + score
-    return sorted(ranked_items, reverse=True, key=lambda x: (x[1], total_scores.get(x[0], 0)))
+    return sorted(
+        ranked_items, reverse=True, key=lambda x: (x[1], total_scores.get(x[0], 0))
+    )
+
 
 # am using this bad boy to test frontend.
 def score_voting(parsed_votes, option_names):
@@ -71,17 +89,19 @@ def score_voting(parsed_votes, option_names):
             totals[name] += score
     return sorted(totals.items(), reverse=True, key=lambda x: x[1])
 
+
 # ============== MAIN ENTRY ==============
+
 
 def calculate_all_results(votes, options, max_score):
     """Calculate results for all voting methods"""
     if not votes or not options:
         return {}
-    
+
     option_names = [o["name"] for o in options]
     parsed = parse_votes(votes, options)
 
     return {
         "score_voting": score_voting(parsed, option_names),
-        "schulze_method" : schulze_method(parsed, option_names)
+        "schulze_method": schulze_method(parsed, option_names),
     }
